03b79c0beec7c177b7208aef94e12616fb4c9a40
Мария <114505489+MariAntonova94@users.noreply.github.com>
2023-08-02 11:21:56 +0700

    Add files via upload

---
+++ ModelElements/Poligon.java
@@ -0,0 +1,4 @@
+import java.util.ArrayList;
+public class Poligon {
+  ArrayList<Point3D> points = new ArrayList<>();
+}
---
+++ ModelElements/PoligonalModel.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+public class PoligonalModel {
+  ArrayList<Poligon> poligons = new ArrayList<>();
+  ArrayList<Texture> textures = new ArrayList<>();
+}
